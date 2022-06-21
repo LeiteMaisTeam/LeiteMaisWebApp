2c056a80616ccb2687c4d9c7dc4854f8ca04ff8f
jjustesen <johannesjustensen99@gmail.com>
2022-06-20 22:51:37 -0300

    feat: 🎸 Add logo on header

--- src/shared/components/Header/Header.jsx
+++ src/shared/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Text, Flex, IconButton } from '@chakra-ui/react'
+import { Flex, IconButton } from '@chakra-ui/react'
 import { HeaderItem } from './style'
 import { menuRoutes } from 'shared/constants/menuRoutes'
 import { useRouter } from 'next/router'
 import { MdExitToApp } from 'react-icons/Md'
 import { useTheme } from 'styled-components'
-
+import LtmLogo from 'shared/assets/Logo'
 export const LtmHeader = () => {
   const { route } = useRouter()
   const theme = useTheme()
@@ -14,9 +14,7 @@ export const LtmHeader = () => {
   return (
     <Flex bg="blue.700" px={64} justifyContent="space-between">
       <Flex alignItems="center">
-        <Text fontWeight={800} fontSize={25} color="white">
-          Leite Mais
-        </Text>
+        <LtmLogo />
       </Flex>
 
       <Flex alignItems="center">
